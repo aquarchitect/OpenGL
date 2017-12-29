4d76ae2dc72163138e8857abb2c5e578ab1ae635
Hai Nguyen <aquarchitecture@gmail.com>
2017-12-29 10:32:46 -0800

    Add playground for quick test

---
+++ Playground/main.cpp
@@ -0,0 +1,17 @@
+//
+//  main.cpp
+//  Playground
+//
+//  Created by Hai Nguyen on 12/29/17.
+//  Copyright © 2017 Hai Nguyen. All rights reserved.
+//
+
+#include <iostream>
+#include <glm/glm.hpp>
+
+using namespace std;
+
+int main(int argc, const char * argv[]) {
+    cout << "Hello World\n";
+    return 0;
+}
