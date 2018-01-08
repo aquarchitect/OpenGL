1b9ba9089c4bb2e52f1aa12342b2db063c4ba6af
Hai Nguyen <aquarchitecture@gmail.com>
2018-01-08 09:52:14 -0800

    Add empty files for obstacles component

---
+++ Demo/Particles/Common/obstacles.cpp
@@ -0,0 +1,9 @@
+//
+//  obstacles.cpp
+//  Particles-iOS
+//
+//  Created by Hai Nguyen on 1/8/18.
+//  Copyright © 2018 Hai Nguyen. All rights reserved.
+//
+
+#include "obstacles.hpp"
---
+++ Demo/Particles/Common/obstacles.hpp
@@ -0,0 +1,14 @@
+//
+//  obstacles.hpp
+//  Particles-iOS
+//
+//  Created by Hai Nguyen on 1/8/18.
+//  Copyright © 2018 Hai Nguyen. All rights reserved.
+//
+
+#ifndef obstacles_hpp
+#define obstacles_hpp
+
+#include <stdio.h>
+
+#endif /* obstacles_hpp */
